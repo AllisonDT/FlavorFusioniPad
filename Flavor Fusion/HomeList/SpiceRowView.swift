296465dfa74ddb1f084a5cfb9231431eed4a0146
Allison Turner <90711802+AllisonDT@users.noreply.github.com>
2024-10-08 17:32:02 -0400

    xcode cloud part 2

--- Flavor Fusion/HomeList/SpiceRowView.swift
+++ Flavor Fusion/HomeList/SpiceRowView.swift
@@ -28,10 +28,9 @@ struct SpiceRowView: View {
     var body: some View {
         GeometryReader { geometry in
             ZStack(alignment: .leading) {
-                // Simple background color
                 RoundedRectangle(cornerRadius: 8)
                     .foregroundColor(spice.isSelected ? Color.blue.opacity(0.3) : Color.gray.opacity(0.1))
-                    .frame(width: geometry.size.width) // Stretch the rectangle to match width
+                    .frame(width: geometry.size.width)
                 
                 SpiceRow(spiceDataViewModel: spiceDataViewModel, spice: spice, isSelecting: isSelecting, recipes: recipes) { selected in
                     onSelect(selected)
